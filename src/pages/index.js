b0f722b7562b3541151ad094e780ab6209a084a7
Portia <plburton@gmail.com>
2019-11-22 13:58:24 +0100

    Added comment pointing to split variable

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const styles = {
-  backgroundColor: '#ff0000',
+  backgroundColor: '#ff0000', // split test variable
   boxShadow: '0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)',
   border: 'none',
   color: 'black',
